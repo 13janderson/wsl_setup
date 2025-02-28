8074375947d8b94fcf63f9d9506a1488fbcd382b
13janderson <jackanderson02@outlook.com>
2025-02-28 23:42:22 +0000

    Restructure dirs for distros

--- install-wsl/wsl-setup.ps1
+++ install-wsl/wsl-setup.ps1
@@ -25,11 +25,10 @@ if($uninstall){
 }
 
 if($setup){
-    # wsl -d $distro --exec bash -c "sudo apt update && sudo apt install -y ansible"
-    wsl -d $distro --exec bash -c "cd; rm -rf dev_setup; git clone https://github.com/13janderson/dev_setup; cd dev_setup; ls"
-    # Push-Location
-    # Set-Location -Path ../wsl/init/docker
-    # wsl -d $distro --exec bash -c install-docker.sh
+    wsl -d $distro --exec bash -c "sudo apt update && sudo apt install -y ansible"
+    wsl -d $distro --exec bash -c "cd; rm -rf dev_setup; git clone https://github.com/13janderson/dev_setup; cd dev_setup; rm -r install-wsl;"
+    wsl -d $distro --exec bash -c "cd wsl/init/docker/$distro; bash ./install-docker.sh"
+
 
 
     # Pop-Location
