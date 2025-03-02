8abb12185ef2943daffc401a04018b93c913f601
13janderson <jackanderson02@outlook.com>
2025-03-02 16:25:12 +0000

    Do gh auth in error loop

--- wsl/init/Ubuntu/setup.sh
+++ wsl/init/Ubuntu/setup.sh
@@ -11,16 +11,20 @@ sudo apt update
 sudo apt install wslu
 
 # git setup using gh cli
-sudo apt update && sudo apt install gh -y
-gh auth login
-gh auth setup-git 
 
 # Prompt user for GitHub username and email
 read -p "Enter your GitHub username: " github_username
 read -p "Enter your GitHub email: " github_email
 
-# Configure Git with the provided username and email
 git config --global user.name "$github_username"
 git config --global user.email "$github_email"
+sudo apt update && sudo apt install gh -y
+
+# gh auth with gh cli
+while ! gh auth login; do 
+    echo "gh auth login timed out. Trying again"
+done
+
+echo "gh auth login success"
 
 
