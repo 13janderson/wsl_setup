cce287df5d39a7fb110d276a7fc120ded6a8c4b6
13janderson <jackanderson02@outlook.com>
2025-03-02 14:44:23 +0000

    Use gh cli for auth

--- install-wsl/wsl-setup.ps1
+++ install-wsl/wsl-setup.ps1
@@ -31,6 +31,7 @@ if($install){
         # wsl -d $distro --exec bash -c "git clone https://github.com/13janderson/dev_setup.git setup; cp -r dotfiles/. ~/" 
         wsl -d $distro --exec bash -c "cd; git clone https://github.com/13janderson/dev_setup.git; cd dev_setup; cd wsl/init/$distro; bash setup.sh"
         wsl -d $distro --exec bash -c "cd; cd dev_setup; cd dotfiles; cp * ~;"
+        wsl -d $distro --exec bash -c "sudo apt update && sudo apt install gh -y"
 
     }
 }
--- wsl/init/Ubuntu/setup.sh
+++ wsl/init/Ubuntu/setup.sh
@@ -18,8 +18,6 @@ read -p "Enter your GitHub email: " github_email
 git config --global user.name "$github_username"
 git config --global user.email "$github_email"
 
-# Setup git credential manager
-# https://learn.microsoft.com/en-us/windows/wsl/tutorials/wsl-git
-# Refers to your windows git credential manager version
-git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager-core.exe"
+sudo apt update && sudo apt install gh -y
+gh auth login
 
