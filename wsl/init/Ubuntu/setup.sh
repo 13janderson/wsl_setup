920a72c83aaa803b7860586a0cb96f359fb21936
13janderson <jackanderson02@outlook.com>
2025-03-02 14:46:20 +0000

    Remove prompts for user credentials directly

--- wsl/init/Ubuntu/setup.sh
+++ wsl/init/Ubuntu/setup.sh
@@ -6,17 +6,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 
-# git setup
-
-# Prompt for GitHub username
-read -p "Enter your GitHub username: " github_username
-
-# Prompt for GitHub email
-read -p "Enter your GitHub email: " github_email
-
-# Configure Git with the provided username and email
-git config --global user.name "$github_username"
-git config --global user.email "$github_email"
+# git setup using gh cli
 
 sudo apt update && sudo apt install gh -y
 gh auth login
