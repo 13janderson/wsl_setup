206a1ce0f3d41ca64df759c6c23fc3156f708068
Jack Anderson <jack.anderson@bjss.com>
2025-03-05 14:08:24 +0000

    Compile scripts to executables and add them to path when first ran

--- wsl-windows/wsl-setup.ps1
+++ wsl-windows/wsl-setup.ps1
@@ -2,34 +2,57 @@ param(
     [switch]$install,
     [switch]$uninstall,
     [switch]$reinstall,
-    [string]$distro = "Ubuntu"
+    [string]$distro = "ubuntu"
 )
 
+# Generate executables and add to path
+$binPath = "$PSScriptRoot\bin"
+$userPath = [Environment]::GetEnvironmentVariable("Path","User")
+if(!$userPath.Contains($binPath)){
+    # Install ps2exe module
+    if($null -eq (Get-Module -ListAvailable -Name ps2exe)){
+        Install-Module -Name ps2exe -Repository PSGallery -Scope CurrentUser
+    }
+    $userPathWithBinPath = "$userPath;$binPath"
+    [Environment]::SetEnvironmentVariable("Path",$userPathWithBinPath,"User")
+    if(!(Test-Path -Path $binPath)){
+        New-Item -ItemType Directory -Path $binPath | Out-Null
+    }
+    $env:path += $binPath
+    $files = Get-ChildItem -Path $PSScriptRoot 
+    $files | ForEach-Object {
+        if($_.Extension -eq ".ps1"){
+            ps2exe $_ -outputFile "$binPath/$($_.Name.Replace($_.Extension, '')).exe"| Out-Null
+        }
+    }
+}else{
+}
+
 if(-not ($install -xor $uninstall -xor $setup)){
-    exit "Only specify one option at once."
+    exit "only specify one option at once."
 }
 
 
 if($install){
     $distros = wsl --list
-    if(-not $distros.Contains($distro)){
+    if(-not $distros.contains($distro)){
         wsl --install -d $distro -n
-        Write-Host "Launching new $distro instance. Provide login details for default linux user and then type 'logout'." -ForegroundColor Green
+        write-host "launching new $distro instance. provide login details for default linux user and then type 'logout'." -foregroundcolor green
 
-        # Set new distro as default
+        # set new distro as default
         wsl --set-default $distro
-        # Launch new instance which should prompt for default username and password.
-        # Currently reliant on user exiting this command for us. logout or exit
+        # launch new instance which should prompt for default username and password.
+        # currently reliant on user exiting this command for us. logout or exit
         wsl -d $distro --cd ~
-        Write-Host "Logged out". -ForegroundColor Green
+        write-host "logged out". -foregroundcolor green
 
-        Write-Host "Running setup script for $distro." -ForegroundColor Green
-        # Pull down setup repo into wsl instance
+        write-host "running setup script for $distro." -foregroundcolor green
+        # pull down setup repo into wsl instance
         wsl -d $distro --exec bash -c "cd; git clone https://github.com/13janderson/dev_setup.git; cd dev_setup; cd wsl-linux/init/$distro; sudo bash setup.sh"
         wsl -d $distro --exec bash -c "cd; cd dev_setup; cd dotfiles; cp .* ~;"
 
-        # Force use to be in docker group so we can run docker commands w/out sudo
-        wsl -d $distro --exec bash -c 'sudo usermod -aG docker $USER'
+        # force use to be in docker group so we can run docker commands w/out sudo
+        wsl -d $distro --exec bash -c 'sudo usermod -ag docker $user'
         wsl -d $distro --shutdown
 
         wsl -d $distro --cd ~
