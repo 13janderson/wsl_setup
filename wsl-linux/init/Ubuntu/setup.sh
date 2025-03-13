427f3300e84e6096e8d8a52b258aa2eec03f01bf
Jack Anderson <jack.anderson@bjss.com>
2025-03-13 22:29:23 +0000

    Use zsh

--- wsl-linux/init/Ubuntu/setup.sh
+++ wsl-linux/init/Ubuntu/setup.sh
@@ -3,12 +3,16 @@
 # Install ansible
 # sudo apt update && sudo apt install -y ansible
 
+# Instal zsh and pre-configure it with oh-my-zsh
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+
 # docker setup
 curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh get-docker.sh; rm get-docker.sh;
 
-
-# git setup using git credential manager
-
+# Git setup using git credential manager
 # Prompt user for GitHub username and email
 read -p "Enter your GitHub username: " github_username
 read -p "Enter your GitHub email: " github_email
