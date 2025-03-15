1f26f2a994dd51d195c398d8659049616533a5ee
Jack Anderson <jack.anderson@bjss.com>
2025-03-15 21:39:00 +0000

    Install gh cli and make user login

--- wsl-linux/init/Ubuntu/setup.sh
+++ wsl-linux/init/Ubuntu/setup.sh
@@ -3,18 +3,15 @@
 # Install ansible
 # sudo apt update && sudo apt install -y ansible
 
-
 # docker setup
 curl -fsSL https://get.docker.com -o get-docker.sh; sudo sh get-docker.sh; rm get-docker.sh;
 
-# Git setup using git credential manager
-# Prompt user for GitHub username and email
-read -p "Enter your GitHub username: " github_username
-read -p "Enter your GitHub email: " github_email
-
-git config --global user.name "$github_username"
-git config --global user.email "$github_email"
-git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-wincred.exe"
+# Install gh cli
+apt-get install gh
+# Preferable to use SSH keys
+gh auth login 
+# gh auth setup-git 
+#git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-wincred.exe"
 
 # Instal zsh and pre-configure it with oh-my-zsh
 apt-get install zsh
