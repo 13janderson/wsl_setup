30a7b0cbdc639d36e375a87f02103d554e66e315
Jack Anderson <jack.anderson@bjss.com>
2025-03-20 10:45:52 +0000

    wsl vpn setup

---
+++ dotfiles/.local/bin/scripts/install-python.sh
@@ -0,0 +1,9 @@
+# Python3 already installed on machine
+# Alias python to python3
+alias python="python3"
+
+# Install pip
+sudo apt-get install python3-pip
+
+# Install poetry
+sudo apt-get install python3-poetry
