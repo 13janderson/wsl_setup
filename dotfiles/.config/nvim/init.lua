41e3a3672cbfecce42f5d44f4015095ff1e72299
Jack Anderson <jack.anderson@bjss.com>
2025-03-27 20:23:55 +0000

    dotfile sync Thu Mar 27 20:23:55 GMT 2025

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -164,6 +164,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('n', '<leader><Tab>', '<C-^>')
 -- FZF current dir
 vim.keymap.set('n', '<leader>pf', ':Files<CR>')
 
@@ -239,6 +240,24 @@ require('lazy').setup {
   -- options to `gitsigns.nvim`.
   --
   -- See `:help gitsigns` to understand what the configuration keys do
+
+  { 'tpope/vim-dadbod' },
+  { 'kristijanhusak/vim-dadbod-completion' },
+  {
+    {
+      'kristijanhusak/vim-dadbod-ui',
+      dependencies = {
+        { 'tpope/vim-dadbod', lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' } }, -- Remove lazy = true here
+      },
+      cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
+      init = function()
+        vim.g.dbs = {
+          nop = 'mysql://root:password@127.0.0.1:3307/CVSBNOP',
+        }
+      end,
+    },
+  },
   {
     'numToStr/Comment.nvim',
     opts = {
@@ -250,24 +269,23 @@ require('lazy').setup {
       },
       ---LHS of operator-pending mappings in NORMAL and VISUAL mode
       opleader = {
-        ---Line-comment keymap
         line = '<C-_>',
         ---Block-comment keymap
       },
     },
   },
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6990/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   'lewis6990/gitsigns.nvim',
+  --   opts = {
+  --     signs = {
+  --       add = { text = '+' },
+  --       change = { text = '~' },
+  --       delete = { text = '_' },
+  --       topdelete = { text = '‾' },
+  --       changedelete = { text = '~' },
+  --     },
+  --   },
+  -- },
   {
     'ibhagwan/fzf-lua',
     -- optional for icon support
@@ -280,46 +298,8 @@ require('lazy').setup {
         keymap = {
           -- Below are the default binds, setting any value in these tables will override
           -- the defaults, to inherit from the defaults change [1] from `false` to `true`
-          builtin = {
-            -- neovim `:tmap` mappings for the fzf win
-            -- true,        -- uncomment to inherit all the below in your custom config
-            ['<M-Esc>'] = 'hide', -- hide fzf-lua, `:FzfLua resume` to continue
-            ['<F1>'] = 'toggle-help',
-            ['<F2>'] = 'toggle-fullscreen',
-            -- Only valid with the 'builtin' previewer
-            ['<F3>'] = 'toggle-preview-wrap',
-            ['<F4>'] = 'toggle-preview',
-            -- Rotate preview clockwise/counter-clockwise
-            ['<F5>'] = 'toggle-preview-ccw',
-            ['<F6>'] = 'toggle-preview-cw',
-            -- `ts-ctx` binds require `nvim-treesitter-context`
-            ['<F7>'] = 'toggle-preview-ts-ctx',
-            ['<F8>'] = 'preview-ts-ctx-dec',
-            ['<F9>'] = 'preview-ts-ctx-inc',
-            ['<S-Left>'] = 'preview-reset',
-            ['<S-down>'] = 'preview-page-down',
-            ['<S-up>'] = 'preview-page-up',
-            ['<M-S-down>'] = 'preview-down',
-            ['<M-S-up>'] = 'preview-up',
-          },
-          fzf = {
-            -- fzf '--bind=' options
-            -- true,        -- uncomment to inherit all the below in your custom config
-            ['ctrl-z'] = 'abort',
-            ['ctrl-u'] = 'unix-line-discard',
-            ['ctrl-f'] = 'half-page-down',
-            ['ctrl-b'] = 'half-page-up',
-            ['ctrl-a'] = 'beginning-of-line',
-            ['ctrl-e'] = 'end-of-line',
-            ['alt-a'] = 'toggle-all',
-            ['alt-g'] = 'first',
-            ['alt-G'] = 'last',
-            -- Only valid with fzf previewers (bat/cat/git/etc)
-            ['f3'] = 'toggle-preview-wrap',
-            ['f4'] = 'toggle-preview',
-            ['shift-down'] = 'preview-page-down',
-            ['shift-up'] = 'preview-page-up',
-          },
+          builtin = {},
+          fzf = {},
         },
       }
     end,
@@ -857,6 +837,12 @@ require('lazy').setup {
           { name = 'nvim_lsp_signature_help' },
         },
       }
+      -- cmp.setup.filetype({ 'sql' }, {
+      --   sources = {
+      --     { name = 'vim-dadbod-completion' },
+      --     { name = 'buffer' },
+      --   },
+      -- })
     end,
   },
 
