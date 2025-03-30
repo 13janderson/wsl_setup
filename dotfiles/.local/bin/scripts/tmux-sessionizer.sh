86701427d04f8d5eaaf86ae12b7c511cb83d8e79
Jack Anderson <jack.anderson@bjss.com>
2025-03-30 21:30:58 +0100

    dotfile sync Sun Mar 30 21:30:58 BST 2025

--- dotfiles/.local/bin/scripts/tmux-sessionizer.sh
+++ dotfiles/.local/bin/scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/projects ~/dev_setup -mindepth 1 -maxdepth 2 -type d -not -path '*/.*' | fzf --preview '')
+    selected=$(find ~/projects ~/dev_setup/ -mindepth 1 -maxdepth 3 -type d -not -path '*/.*' | fzf --preview '')
 fi
 
 if [[ -z $selected ]]; then
