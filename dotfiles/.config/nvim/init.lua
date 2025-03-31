03e8d9dec2351fd3f51db77e6db715e604cc631b
Jack Anderson <jack.anderson@bjss.com>
2025-03-31 08:56:59 +0100

    dotfile sync Mon Mar 31 08:56:59 BST 2025

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -323,6 +323,7 @@ require('lazy').setup {
         harpoon.setup {}
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        -- Pseudo arrow keys for config
         vim.keymap.set("n", "<C-i>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
