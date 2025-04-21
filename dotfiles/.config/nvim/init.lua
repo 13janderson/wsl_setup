da3a8f2d92aeb9de10d8c59ee08c1028e84d9e68
Jack Anderson <jack.anderson@bjss.com>
2025-04-21 21:41:02 +0100

    dotfile sync Mon Apr 21 21:41:02 BST 2025

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -105,8 +105,6 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 
--- Vertical splits
-vim.opt.splitright = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
