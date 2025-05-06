928aa9cebab0b223c91fcdd59507848efb4a8cc4
Jack Anderson <jack.anderson@bjss.com>
2025-05-06 15:42:03 +0100

    dfd working with reload

---
+++ dotfiles/.local/bin/scripts/dfd.sh
@@ -0,0 +1,17 @@
+# Only want to copy files tracked by git
+dfd ()
+{
+  cd ~/dev_setup/dotfiles
+  git ls-files | while IFS= read -r df; do
+    # Copy all files tracked by git
+    cp "$df" "$HOME/$df"
+  done
+  cd - 
+}
+
+dfd > /dev/null 2>&1
+ 
+# Reload shell
+echo "dotfiles reloaded"
+echo "test"
+exec $SHELL
