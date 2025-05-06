fb0d6afc50aca9150cf3440bc1999b6976b9e8fa
Jack Anderson <jack.anderson@bjss.com>
2025-05-06 19:55:04 +0100

    Add dfu script

---
+++ dotfiles/.local/bin/scripts/dfu.sh
@@ -0,0 +1,9 @@
+cd ~/dev_setup/dotfiles
+git ls-files | while IFS= read -r df; do
+# Copy all files tracked by git from root directory
+cp "$HOME/$df" "$df"
+done
+git commit -am "dotfile sync $(date)"
+git push
+cd - 
+
