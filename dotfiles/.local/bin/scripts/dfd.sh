f5b29ba1a2aae9f09fb57cb0e57703855b24f55a
Jack Anderson <jack.anderson@bjss.com>
2025-05-06 15:54:56 +0100

    dotfile sync Tue May  6 15:54:56 BST 2025

--- dotfiles/.local/bin/scripts/dfd.sh
+++ dotfiles/.local/bin/scripts/dfd.sh
@@ -1,16 +1,15 @@
 # Only want to copy files tracked by git
-dfd ()
-{
-  cd ~/dev_setup/dotfiles
-  git ls-files | while IFS= read -r df; do
-    # Copy all files tracked by git
-    cp "$df" "$HOME/$df"
-  done
-  cd - 
-}
+# dfd ()
+# {
+cd ~/dev_setup/dotfiles
+git ls-files | while IFS= read -r df; do
+# Copy all files tracked by git
+cp "$df" "$HOME/$df"
+done
+cd - 
+# }
 
-dfd > /dev/null 2>&1
  
 # Reload shell
 echo "dotfiles reloaded"
-exec $SHELL
+source ~/.zshrc
