2a3034ef230fcf3904cad5042ace8a15f61423a1
Jack Anderson <jack.anderson@bjss.com>
2025-05-06 19:55:27 +0100

    dotfile sync Tue May  6 19:55:27 BST 2025

--- dotfiles/.local/bin/scripts/dfu.sh
+++ dotfiles/.local/bin/scripts/dfu.sh
@@ -1,9 +1,12 @@
-cd ~/dev_setup/dotfiles
-git ls-files | while IFS= read -r df; do
-# Copy all files tracked by git from root directory
-cp "$HOME/$df" "$df"
-done
-git commit -am "dotfile sync $(date)"
-git push
-cd - 
+dfu () {
+  cd ~/dev_setup/dotfiles
+  git ls-files | while IFS= read -r df; do
+    # Copy all files tracked by git from root directory
+    cp "$HOME/$df" "$df"
+  done
+  git commit -am "dotfile sync $(date)"
+  git push
+  cd - 
+}
 
+dfu
