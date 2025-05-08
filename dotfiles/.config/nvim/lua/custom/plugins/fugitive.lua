16523fbb63c3120019d350360e3a34342d496fe3
Jack Anderson <jack.anderson@bjss.com>
2025-05-08 16:30:56 +0100

    feat: CR

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -16,7 +16,7 @@ return{
     vim.keymap.set("n", "<leader>c", function ()
       vim.cmd("silent! G commit -a")
       vim.api.nvim_buf_set_lines(0, 0, 1, true, {"feat: "})
-      vim.cmd("<CR>")
+      vim.api.nvim_feedkeys("<CR>", "n", true)
     end)
   end
 
