0146942d1ca8407d023702b959fe589821626ed4
Jack Anderson <jack.anderson@bjss.com>
2025-05-08 15:32:13 +0100

    feature:

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -5,6 +5,18 @@ return{
     -- Set keybindings for resolving merge conflicts
     vim.keymap.set("n", "<leader>h", ":diffget //2<CR>")
     vim.keymap.set("n", "<leader>l", ":diffget //3<CR>")
+    vim.keymap.set("n", "<leader>p", function ()
+      vim.cmd("silent G push")
+      print("Fugitive ↑")
+    end)
+    vim.keymap.set("n", "<leader>P", function ()
+      vim.cmd("silent G pull")
+      print("Fugitive ↓")
+    end)
+    vim.keymap.set("n", "<leader>c", function ()
+      vim.cmd("G commit -a")
+      vim.api.nvim_buf_set_lines(0, 1, 1, false, {"feature:"})
+    end)
   end
 
 }
