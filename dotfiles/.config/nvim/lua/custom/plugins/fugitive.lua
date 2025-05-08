0bed82b9ceaf6e25e4cff232019ad5aae97014ae
Jack Anderson <jack.anderson@bjss.com>
2025-05-08 16:22:22 +0100

    feat: set buf lines with feat

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -14,8 +14,9 @@ return{
       print("Fugitive ↓")
     end)
     vim.keymap.set("n", "<leader>c", function ()
-      vim.cmd("G commit -a")
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("ifeature: ", true, false, true), "n", false)
+      vim.cmd("silent G commit -a")
+      vim.api.nvim_buf_set_lines(0, 0, 1, true, {"feat: "})
+      -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("ifeature: ", true, false, true), "n", false)
     end)
   end
 
