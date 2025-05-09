00d41a19fdd777c0f8f048f72a36a636192d275e
Jack Anderson <credential.email>
2025-05-09 09:51:38 +0100

    feat: require globals in init.lua, all seems good now

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -282,6 +282,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+require("globals")
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
     -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
