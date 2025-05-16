bcde83d9489f38257175a3695a96bde1f84cbd47
Jack Anderson <jackanderson02@outlook.com>
2025-05-16 17:32:00 +0100

    feat: work email

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -257,7 +257,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
           highlight CursorLineNr guifg=#ff8800 gui=bold
           highlight DiffAdd    guibg=#003300 guifg=#00ff00
           highlight DiffDelete guibg=#330000 guifg=#ff4444
-      ]]
+      ]] 
   end,
 })
 
