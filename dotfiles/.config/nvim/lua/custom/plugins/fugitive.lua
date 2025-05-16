8b4d588d0fb4d91922f8b4ee20afc1bb682bd028
Jack Anderson <jackanderson02@outlook.com>
2025-05-16 23:31:47 +0100

    feat: latest fugitive keybindings

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -3,8 +3,8 @@ return{
   config = function(_)
     -- Use navigating merge conflict defaults for now [c for previous ]c for next conflict
     -- Set keybindings for resolving merge conflicts
-    vim.keymap.set("n", "<leader>h", ":diffget //2<CR>")
-    vim.keymap.set("n", "<leader>l", ":diffget //3<CR>")
+    vim.keymap.set("n", "<leader>gh", ":diffget //2<CR>")
+    vim.keymap.set("n", "<leader>gl", ":diffget //3<CR>")
     vim.keymap.set("n", "<leader>p", function ()
       vim.cmd("G push")
       Clear(250)
@@ -13,7 +13,7 @@ return{
       vim.cmd("G pull")
       Clear(250)
     end)
-    vim.keymap.set("n", "<leader>c", function ()
+    vim.keymap.set("n", "<leader>C", function ()
       vim.cmd("G commit -a")
       vim.api.nvim_buf_set_lines(0, 0, 1, true, {"feat: "})
     end)
