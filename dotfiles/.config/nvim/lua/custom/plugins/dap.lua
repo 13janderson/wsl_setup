a4792913251878d22ae8adc8dba8ca5a46329a8d
Jack Anderson <jackanderson02@outlook.com>
2025-05-23 16:14:50 +0100

    feat: dap ts wip, at least runs but can't go through breakpoints

--- dotfiles/.config/nvim/lua/custom/plugins/dap.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/dap.lua
@@ -1,130 +1,94 @@
-return
+return {
   {
-    {
-      "mfussenegger/nvim-dap",
-      lazy = true,
-      -- Copied from LazyVim/lua/lazyvim/plugins/extras/dap/core.lua and modified.
-      keys = {
-        {
-          "<leader>db",
-          function() require("dap").toggle_breakpoint() end,
-          desc = "Toggle Breakpoint",
-        },
-        {
-          "<leader>dc",
-          function() require("dap").continue() end,
-          desc = "Continue",
-        },
-        {
-          "<leader>dC",
-          function() require("dap").run_to_cursor() end,
-          desc = "Run to Cursor",
-        },
-        {
-          "<leader>dT",
-          function() require("dap").terminate() end,
-          desc = "Terminate",
-        },
-      },
-      config = function()
-        local dap = require("dap")
+    "mfussenegger/nvim-dap",
 
-        -- Configure js-debug-adapter for TypeScript/JavaScript
-        dap.adapters["pwa-node"] = {
-          type = "server",
-          host = "localhost",
-          port = "${port}", -- Let DAP pick a free port
-          executable = {
-            command = "js-debug-adapter", -- Provided by mason-nvim-dap
-            args = { "${port}" },
-          },
-        }
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
 
-        for _, language in ipairs { "typescript", "javascript" } do
-          dap.configurations[language] = {
-            {
-              type = "pwa-node",
-              request = "launch",
-              name = "Launch file",
-              program = "${file}",
-              cwd = "${workspaceFolder}",
-              runtimeExecutable = "ts-node",
-              sourceMaps = true, -- Enable source maps
-              resolveSourceMapLocations = { -- Specify where to find source maps
-                "${workspaceFolder}/**",
-                "!**/node_modules/**",
-              },
-            },
-          }
-        end
+    -- Plugin configuration
+    config = function()
+      local dap = require("dap")
+
+      -- -- Configure Mason for managing debug adapters with auto-install
+      -- require("mason").setup({
+      --   ensure_installed = { "js-debug-adapter" }, -- Auto-install js-debug-adapter
+      -- })
+      --
+      -- -- Check if ts-node is installed
+      -- local ts_node_path = vim.fn.exepath("ts-node")
+      -- if ts_node_path == "" then
+      --   print("Error: ts-node not found. Please install it with 'npm install -g ts-node'")
+      --   return
+      -- end
+      --
+      -- -- Construct js-debug-adapter path
+      -- local js_debug_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
+      --
+      -- -- Check if the adapter executable exists
+      -- if not vim.loop.fs_stat(js_debug_path) then
+      --   print("Error: js-debug-adapter not found at " .. js_debug_path)
+      --   print("Please ensure js-debug-adapter is installed via :MasonInstall js-debug-adapter and Mason has completed installation")
+      --   return
+      -- end
+
+      -- Configure DAP adapter for JavaScript/TypeScript (vscode-js-debug)
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "8123",
+        executable = {
+          command = "js-debug-adapter",
+        },
+      }
+
+      -- Configuration for TypeScript debugging
+      dap.configurations.typescript = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch File",
+          cwd = "${workspaceFolder}",
+          runtimeExecutable = "node", -- Use resolved ts-node path
+          sourceMaps = true,
+          protocol = "inspector",
+          console = "integratedTerminal",
+          outFiles = { "${workspaceFolder}/**/*.js" },
+          skipFiles = { "<node_internals>/**", "node_modules/**" },
+        },
+      }
+
+      -- Basic DAP keybindings
+      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
+      vim.keymap.set("n", "<leader>dv", dap.step_over, { desc = "DAP: Step Over" })
+      vim.keymap.set("n", "<leader>do", dap.step_into, { desc = "DAP: Step Into" })
+      vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "DAP: Step Out" })
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "nvim-neotest/nvim-nio"
+    },
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function() 
+        dapui.open()
       end
-    },
-    {
-      "jay-babu/mason-nvim-dap.nvim",
-      ---@type MasonNvimDapSettings
-      opts = {
-        -- This line is essential to making automatic installation work
-        -- :exploding-brain
-        handlers = {},
-        automatic_installation = {
-          -- These will be configured by separate plugins.
-          exclude = {
-          },
-        },
-        -- DAP servers: Mason will be invoked to install these if necessary.
-        ensure_installed = {
-          "python",
-          "js",
-          "go",
-        },
-      },
-      dependencies = {
-        "mfussenegger/nvim-dap",
-        "williamboman/mason.nvim",
-      },
-    },
-    {
-      "theHamsta/nvim-dap-virtual-text",
-      config = true,
-      dependencies = {
-        "mfussenegger/nvim-dap",
-      },
-    },
-    {
-      "rcarriga/nvim-dap-ui",
-      config = function ()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        dapui.setup()
 
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-          dapui.open()
-        end
+      dap.listeners.after.event_terminated["dapui_config"] = function() 
+        dapui.close()
+      end
+      
+      dap.listeners.after.event_exited["dapui_config"] = function() 
+        dapui.close()
+      end
 
-        dap.listeners.after.event_terminated["dapui_config"] = function()
-          dapui.close()
-        end
-
-        dap.listeners.after.event_exited["dapui_config"] = function()
-          dapui.close()
-        end
-
-      end,
-      keys = {
-        {
-          "<leader>du",
-          function()
-            require("dapui").toggle({})
-          end,
-          desc = "Dap UI"
-        },
-      },
-      dependencies = {
-        "jay-babu/mason-nvim-dap.nvim",
-        "leoluz/nvim-dap-go",
-        "mfussenegger/nvim-dap-python",
-        "nvim-neotest/nvim-nio",
-        "theHamsta/nvim-dap-virtual-text",
-      },
-    }
+      vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "DAPUI: Toggle" })
+    end
   }
+}
