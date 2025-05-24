f8364bba7f4c62133afb0f5557bb5da8886348ac
Jack Anderson <jackanderson02@outlook.com>
2025-05-24 20:48:54 +0100

    feat: install npm globals

--- dotfiles/.local/bin/scripts/install-jsts.sh
+++ dotfiles/.local/bin/scripts/install-jsts.sh
@@ -1,9 +1,10 @@
-# Install node for use in ts_ls
-# curl -o- https://fnm.vercel.app/install | bash
-# fnm install 22
+# Install node 
+curl -o- https://fnm.vercel.app/install | bash
+fnm install 22
+npm install -g typescript ts-node tsx
 
 
 # Install bun as better general purpose alternative to node js
-# curl -fsSL https://bun.sh/install | bash 
+curl -fsSL https://bun.sh/install | bash 
 # Manually add bun to path in .zshrc
 echo 'export PATH="$PATH:$HOME/.bun/bin"' >> ~/.zshrc
