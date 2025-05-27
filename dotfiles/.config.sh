f957e4a4020d2338ac69079301a34d257d249311
Jack Anderson <jackanderson02@outlook.com>
2025-05-27 22:23:01 +0100

    feat: empty config

--- dotfiles/.config.sh
+++ dotfiles/.config.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 # Add branch specific configurations here
 
-# Launch Windows chrome as browser
-export BROWSER="chrome-wsl.sh"
-
-# bun completions
-[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
-# Add bun to path
-export PATH="$PATH:$HOME/.bun/bin"
-
-# fnm
-FNM_PATH="$HOME/.local/share/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="$HOME/.local/share/fnm:$PATH"
-  eval "$(fnm env)"
-fi
-
-. "/home/jackanderson/.deno/env"
