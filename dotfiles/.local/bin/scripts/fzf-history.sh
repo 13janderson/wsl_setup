71522df11e204f8b8e713f8cd2da9cc56e2496f3
Jack Anderson <jackanderson02@outlook.com>
2025-05-27 13:04:18 +0100

    feat: copy fzf-history to clipboard as well

--- dotfiles/.local/bin/scripts/fzf-history.sh
+++ dotfiles/.local/bin/scripts/fzf-history.sh
@@ -5,22 +5,24 @@ selected=$(tac ~/.zsh_history | sed -E "/$me/d" | sed -E 's/(.*;)(.*)/\2/' | fzf
 echo "> $selected"
 eval "$selected"
 
+echo "$selected" | xclip -se clip
+
 # Check if the selected command is empty
-if [ -z "$selected" ]; then
-  echo "No command selected, exiting."
-  exit 1
-fi
-
-# Write the command to a temporary file, escaping special characters
-temp_file="/tmp/run_command.sh"
-echo "#!/bin/bash" > "$temp_file"
-printf '%s\n' "$selected" >> "$temp_file"
-
-# Make the temporary file executable
-chmod +x "$temp_file"
-
-# Source the temporary file to run the command in the current shell
-source "$temp_file"
-
-# Clean up
-rm -f "$temp_file"
+# if [ -z "$selected" ]; then
+#   echo "No command selected, exiting."
+#   exit 1
+# fi
+#
+# # Write the command to a temporary file, escaping special characters
+# temp_file="/tmp/run_command.sh"
+# echo "#!/bin/bash" > "$temp_file"
+# printf '%s\n' "$selected" >> "$temp_file"
+#
+# # Make the temporary file executable
+# chmod +x "$temp_file"
+#
+# # Source the temporary file to run the command in the current shell
+# source "$temp_file"
+#
+# # Clean up
+# rm -f "$temp_file"
