e0aa48694a410021fca9062a0a40b843bf4d7038
Jack Anderson <jackanderson02@outlook.com>
2025-05-27 22:59:40 +0100

    feat: print when no changes, cleanup old fugitive keymaps

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -5,22 +5,16 @@ return {
     -- Set keybindings for resolving merge conflicts
     vim.keymap.set("n", "<leader>(", ":diffget //2<CR>")
     vim.keymap.set("n", "<leader>)", ":diffget //3<CR>")
-    -- vim.keymap.set("n", "<leader>p", function ()
-    --   vim.cmd("G push")
-    --   Clear(250)
-    -- end)
-    -- vim.keymap.set("n", "<leader>P", function ()
-    --   vim.cmd("G pull")
-    --   Clear(250)
-    -- end)
     vim.keymap.set("n", "<leader>c", function()
       -- local diff = vim.cmd("G diff")
       local status = vim.fn.execute("G status --untracked-files=no --porcelain", 'silent')
       local status_len = string.len(status)
-      print(status_len)
       if status_len > 1 then
         vim.cmd("G commit -a --no-verify")
         vim.api.nvim_buf_set_lines(0, 0, 1, true, { "feat: " })
+      else
+        print("No changes to commit")
+        Clear(250)
       end
     end)
   end
