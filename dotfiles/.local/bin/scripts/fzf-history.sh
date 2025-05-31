2ff69439ae97131210096360e19d4de203de8e0d
Jack Anderson <jackanderson02@outlook.com>
2025-05-31 11:20:58 +0100

    feat: detect if inside tmux, do not use widget anymore

--- dotfiles/.local/bin/scripts/fzf-history.sh
+++ dotfiles/.local/bin/scripts/fzf-history.sh
@@ -1,13 +1,13 @@
-#!bin/zsh
-
-fzf-history-widget() {
-  me=$(basename "$0")
-  selected=$(tac ~/.zsh_history | sed -E "/$me/d" | sed -E 's/(.*;)(.*)/\2/' | fzf)
-  if [[ -n "$selected" ]]; then
-    # Send keys to current tmux window and hit enter, I love tmux
+#!/bin/bash
+me=$(basename "$0")
+selected=$(tac ~/.zsh_history | sed -E "/$me/d" | sed -E 's/(.*;)(.*)/\2/' | fzf)
+if [[ -n "$selected" ]]; then
+  # Send keys to current tmux window and hit enter, I love tmux.
+  # only do this if we are inside a tmux session
+  if [  "$TMUX" ]; then
     tmux send-keys "$selected" C-m
+  else
+    eval $selected
   fi
-}
+fi
 
-zle -N fzf-history-widget
-bindkey '^R' fzf-history-widget
