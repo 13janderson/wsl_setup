8ae1514ea54cdb7332e1ae0eb070f23566beeb15
Jack Anderson <jackanderson02@outlook.com>
2025-06-02 10:33:41 +0100

    feat: revert use of bufnr

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -7,8 +7,7 @@ return {
     vim.keymap.set("n", "<leader>)", ":diffget //3<CR>")
     vim.keymap.set("n", "<leader>c", function()
       vim.cmd("G commit -a --no-verify")
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.api.nvim_buf_set_lines(bufnr, 0, 1, true, { "feat: " })
+      vim.api.nvim_buf_set_lines(0, 0, 1, true, { "feat: " })
     end)
   end
 
