b685bdabbc04d3990f6888bb852fed33d6324eea
Jack Anderson <jackanderson02@outlook.com>
2025-06-02 10:30:34 +0100

    wip: set cmdheight and cmdwinheight

--- dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -6,10 +6,11 @@ return {
     vim.keymap.set("n", "<leader>(", ":diffget //2<CR>")
     vim.keymap.set("n", "<leader>)", ":diffget //3<CR>")
     vim.keymap.set("n", "<leader>c", function()
+      vim.opt.cmdheight = 1
+      vim.opt.cmdwinheight = 1
       vim.cmd("G commit -a --no-verify")
-      vim.api.nvim_buf_set_lines(0, 0, 1, true, { "feat: " })
-      print("Done")
-      vim.api.nvim_feedkeys(" ", "n", true)
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.api.nvim_buf_set_lines(bufnr, 0, 1, true, { "feat: " })
     end)
   end
 
