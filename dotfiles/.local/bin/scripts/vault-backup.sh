3bc79637b1096ef2f2da1a7268083ca3df0dd333
Jack Anderson <jackanderson02@outlook.com>
2025-06-03 09:26:16 +0100

    feat: janderson backup vault. ghs local

--- dotfiles/.local/bin/scripts/vault-backup.sh
+++ dotfiles/.local/bin/scripts/vault-backup.sh
@@ -2,6 +2,15 @@
 # For proper use, add this to crontab with crontab -e
 # Perform backup of entire vault, not just anything tracked by git
 # * * * * * ~/.local/bin/scripts/vault-backup.sh
+while [ true ]
+do
+    source ~/.local/bin/scripts/ghswitch.sh
+    git_email=$(git config --local user.email)
+    echo $git_email
+  if [[ "$git_email" == "jackanderson02@outlook.com" ]]; then
+      break
+  fi
+done
 cd ~/vault/
 git add .
 timestamp=$(date)
