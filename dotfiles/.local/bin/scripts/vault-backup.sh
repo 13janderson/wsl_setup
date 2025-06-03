9aa51f957e5afa5db3d8d93efdbbd1e4a041d962
Jack Anderson <jackanderson02@outlook.com>
2025-06-03 16:02:14 +0100

    feat: remove echo, strange LB at end of gh/hosts.yaml what the fuck

--- dotfiles/.local/bin/scripts/vault-backup.sh
+++ dotfiles/.local/bin/scripts/vault-backup.sh
@@ -2,16 +2,15 @@
 # For proper use, add this to crontab with crontab -e
 # Perform backup of entire vault, not just anything tracked by git
 # * * * * * ~/.local/bin/scripts/vault-backup.sh
+cd ~/vault/
 while [ true ]
 do
     source ~/.local/bin/scripts/ghswitch.sh
     git_email=$(git config --local user.email)
-    echo $git_email
-  if [[ "$git_email" == "jackanderson02@outlook.com" ]]; then
-      break
-  fi
+    if [[ "$git_email" == "jackanderson02@outlook.com" ]]; then
+        break
+    fi
 done
-cd ~/vault/
 git add .
 timestamp=$(date)
 git commit -am "wsl vault backup: ${timestamp}"
