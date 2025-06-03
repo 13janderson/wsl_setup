b07e55819f41538d0238f2d7faafc4ae3813ff7e
Jack Anderson <jackanderson02@outlook.com>
2025-06-03 09:13:01 +0100

    dotfile sync Tue Jun  3 09:13:01 BST 2025

--- dotfiles/.local/bin/scripts/vault-backup.sh
+++ dotfiles/.local/bin/scripts/vault-backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# For proper use, add this to crontab with crontab -e
 # Perform backup of entire vault, not just anything tracked by git
+# * * * * * ~/.local/bin/scripts/vault-backup.sh
 cd ~/vault/
 git add .
 timestamp=$(date)
