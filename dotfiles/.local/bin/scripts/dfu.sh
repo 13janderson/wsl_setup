9ab260315d9aff0352439a8fe33eb41c4826d1d3
Jack Anderson <jackanderson02@outlook.com>
2025-06-08 22:51:50 +0100

    dotfile sync Sun Jun  8 22:51:50 BST 2025

--- dotfiles/.local/bin/scripts/dfu.sh
+++ dotfiles/.local/bin/scripts/dfu.sh
@@ -12,12 +12,7 @@ dfu () {
 
     # If file is a symlink, read the contents of the file to a local copy
     if [ -h $df ]; then
-      symlimk_df="$symlink_dir/$df"
-      echo "$symlink_dir"
-      echo "$df"
-      echo "$symlink_dir/$df"
-
-      cp $(readlink $df) $symlink_df 
+      cp $(readlink $df) $symlink_dr/$df
       # Ensure this file is added to git
       git add $symlink_df > /dev/null 2>&1
     else
