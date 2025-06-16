4798644a5663006bdb1c4153247a3a06aecb83b9
janderson <jackanderson02@outlook.com>
2025-06-16 22:02:41 +0100

    feat: fzf-history unique entries

--- dotfiles/.local/bin/scripts/fzf-history.sh
+++ dotfiles/.local/bin/scripts/fzf-history.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 me=$(basename "$0")
-selected=$(tac ~/.zsh_history | sed -E "/$me/d" | sed -E 's/(.*;)(.*)/\2/' | fzf)
+selected=$(tac ~/.zsh_history | sed -E "/$me/d" | sed -E 's/(.*;)(.*)/\2/' | awk '!seen[$0]++' | fzf)
+
 if [[ -n "$selected" ]]; then
   # Send keys to current tmux window and hit enter, I love tmux.
   # only do this if we are inside a tmux session
