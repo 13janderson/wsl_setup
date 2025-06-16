6e9aea626629986afac02f74d1710e11cc33049d
janderson <jackanderson02@outlook.com>
2025-06-16 13:52:28 +0100

    dotfile sync Mon Jun 16 13:52:28 BST 2025

--- dotfiles/.local/bin/scripts/dfu.sh
+++ dotfiles/.local/bin/scripts/dfu.sh
@@ -2,11 +2,9 @@
 encrypt=(".zsh_history")
 
 if [ ${#encrypt[@]} -gt 0 ]; then
-  echo "Enter encryption password: "
-  pass=$(read  -s)
+  echo "Enter encryption password:"
+  read -s pass
 fi
-
-
 
 cd ~/dev_setup/dotfiles
 symlink_dir=.symlinks
@@ -17,20 +15,21 @@ git ls-files | while IFS= read -r df; do
   fi
 
   if [[ ${encrypt[@]} =~ $df ]]; then
-    openssl aes-256-cbc -pbkdf2 -salt -a -e -in -passin $HOME/$df -out $df -pass pass:$pass
+    openssl aes-256-cbc -pbkdf2 -salt -a -e -in $HOME/$df -out $df -k $pass
     continue
   fi
 
   # # If file is a symlink, read the contents of the file to a local copy
-  # if [ -h $df ]; then
-  #   cp $() $symlink_dir
-  #   # Ensure this file is added to git
-  #   git add $symlink_df > /dev/null 2>&1
-  # else
-  #   # Copy all files tracked by git from root directory
-  #   cp "$HOME/$df" "$df"
-  # fi
+  if [ -h $df ]; then
+    cp $() $symlink_dir
+    # Ensure this file is added to git
+    git add $symlink_df > /dev/null 2>&1
+  else
+    # Copy all files tracked by git from root directory
+    cp "$HOME/$df" "$df"
+  fi
 done
+
 git commit -am "dotfile sync $(date)"
 git push
 cd - 
