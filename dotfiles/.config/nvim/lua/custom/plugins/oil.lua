b7b60de6fb1797d96c687108522fcc13ca60c29e
janderson <jackanderson02@outlook.com>
2025-07-31 22:07:44 +0100

    feat: oil cwd winbar

--- dotfiles/.config/nvim/lua/custom/plugins/oil.lua
+++ dotfiles/.config/nvim/lua/custom/plugins/oil.lua
@@ -1,3 +1,14 @@
+function Get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
   {
     'stevearc/oil.nvim',
@@ -7,6 +18,7 @@ return {
       default_file_explore = true,
       skip_confirm_for_simple_edits = true,
       prompt_save_on_select_new_entry = true,
+      -- This is fucking beautiful
       git = {
         -- add = function(_) return true end,
         mv = function(_) return true end,
@@ -28,10 +40,13 @@ return {
         ["gs"] = { "actions.change_sort", mode = "n" },
         ["gx"] = "actions.open_external",
         ["g."] = { "actions.toggle_hidden", mode = "n" },
-        -- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
       },
       view_options = {
         show_hidden = true,
+      },
+      win_options = {
+        winbar = "%!v:lua.Get_oil_winbar()",
       }
     },
     -- Optional dependencies
