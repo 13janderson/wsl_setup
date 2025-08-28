f400b055a55a7201d8b94c14eed53daeefb8f432
janderson <jackanderson02@outlook.com>
2025-08-28 20:56:58 +0100

    remove dotfiles to make way for submodule

--- dotfiles/.local/bin/scripts/chrome-wsl.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-"/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" --new-tab "$@"
--- dotfiles/.local/bin/scripts/install-nvim.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update
-sudo apt install make gcc ripgrep unzip git xclip neovim
